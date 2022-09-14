cc943ae8de9be8be1e187ae728fa7d618b5f843f
xujunming <xujunming@apusapps.com>
2022-09-14 23:51:31 +0800

    更新版本

--- app/src/main/java/com/alan/pluginhost/MainActivity.java
+++ app/src/main/java/com/alan/pluginhost/MainActivity.java
@@ -14,20 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (RePlugin.isPluginInstalled("loan")) {
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (RePlugin.isPluginInstalled("loan")) {
-                    RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("loan", "cわㄚ.ぷゾ.ㅂㅞㆫ.ㆧほにㆹ"));
-                } else {
-                    Toast.makeText(MainActivity.this, "You must install demo3 first!", Toast.LENGTH_SHORT).show();
-                }
+            RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("loan", "cわㄚ.ぷゾ.ㅂㅞㆫ.ゼナシ"));
+//                    RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("loan", "cわㄚ.ぷゾ.ㅂㅞㆫ.ㆧほにㆹ"));
+            finish();
+        } else {
+            Toast.makeText(MainActivity.this, "You must install demo3 first!", Toast.LENGTH_SHORT).show();
+        }
 
 //                RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("com.alan.pluglib", "com.alan.pluglib.CodeActivity"));
 
-            }
-        });
+//        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
     }
 }
