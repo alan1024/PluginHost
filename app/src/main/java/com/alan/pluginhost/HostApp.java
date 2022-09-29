a56ee71cc82d8d0b88e93d545687bc8cb31157ba
许君明 <gw00306182@tech.gwm.cn>
2022-09-29 17:43:04 +0800

    增加AppsFlyer

--- app/src/main/java/com/alan/pluginhost/HostApp.java
+++ app/src/main/java/com/alan/pluginhost/HostApp.java
@@ -1,14 +1,57 @@
 package com.alan.pluginhost;
 
+import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
 import com.qihoo360.replugin.RePluginApplication;
 
+import java.util.Map;
+
 public class HostApp extends RePluginApplication {
+    public static final String AF_KEY = "GFMADKCDDtzsUubS58QVGA";
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        init(this, AF_KEY);
+    }
+
+    public void init(Application context, String af_dev_key) {
+        Log.e("xujm", "AppsFlyer注册key:" + af_dev_key);
+        AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
+            @Override
+            public void onConversionDataSuccess(Map<String, Object> conversionData) {
+                Log.e("xujm", "AppsFlyer初始化成功");
+            }
+
+            @Override
+            public void onConversionDataFail(String errorMessage) {
+                Log.e("xujm", "AppsFlyer初始化失败" + errorMessage);
+            }
+
+            @Override
+            public void onAppOpenAttribution(Map<String, String> conversionData) {
+                Log.e("xujm", "AppsFlyer属性成功：" + conversionData.toString());
+
+            }
+
+            @Override
+            public void onAttributionFailure(String errorMessage) {
+                Log.e("xujm", "AppsFlyer属性失败：" + errorMessage);
+            }
+        };
+        AppsFlyerLib.getInstance().init(af_dev_key, conversionListener, context);
+        AppsFlyerLib.getInstance().setDebugLog(true);
+        AppsFlyerLib.getInstance().start(context);
     }
 
 
