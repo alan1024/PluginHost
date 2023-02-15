4a5cae3fab250efc0fdc927004e7fd19db4857c2
xujunming <xujunming@apusapps.com>
2023-02-15 22:38:37 +0800

    RupeePark第四套宿主包完整版

--- app/src/main/java/com/alan/pluginhost/HostApp.java
+++ app/src/main/java/com/alan/pluginhost/HostApp.java
@@ -1,5 +1,6 @@
 package com.alan.pluginhost;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
@@ -13,15 +14,15 @@ import com.alan.pluginhost.google.entity.FireBase;
 import com.alan.pluginhost.google.entity.Referrer;
 import com.appsflyer.AppsFlyerConversionListener;
 import com.appsflyer.AppsFlyerLib;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.qihoo360.replugin.RePluginApplication;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Map;
 
 public class HostApp extends RePluginApplication {
-    public static final String AF_KEY = "BjbZ8Y5WdjqF9Vu7mxYqeW";
+    public static final String AF_KEY = "9rUyyRzEAXJptHrBt3b4Xc";
     public static volatile HostApp instance;
 
     public static FireBase fireBase;
@@ -64,10 +65,11 @@ public class HostApp extends RePluginApplication {
         super.onCreate();
         instance = this;
         Log.e("xujm", "宿主创建");
-        if (TextUtils.equals(getCurrentProcessName(), getPackageName())) {
+        if (isMainProcess()) {
             Log.e("xujm", "宿主当前进程SDK初始化");
             initReferrer(this);
-//            initAF(this, AF_KEY);
+            initAF(this, AF_KEY);
+            initFirebase(this);
         }
     }
 
@@ -95,7 +97,7 @@ public class HostApp extends RePluginApplication {
                 Log.e("xujm", "AppsFlyer初始化成功 appsFlyerUID:" + appsFlyerUID);
 
                 Intent customIntent = new Intent();
-                customIntent.setAction("com.alan.receiver.action.AFRECEIVER");
+                customIntent.setAction("com.rupee.park.action.AFRECEIVER");
                 customIntent.putExtra(BR_ACTION, ACTION_AF);
                 customIntent.putExtra(KEY_APPSFLYERUID, appsFlyerUID);
                 sendBroadcast(customIntent);
@@ -123,33 +125,80 @@ public class HostApp extends RePluginApplication {
     }
 
 
+    /**
+     * 初始化Firebase
+     *
+     * @param application
+     */
+    private void initFirebase(Application application) {
+        FirebaseApp.initializeApp(application);
+
+        final String[] token = {""};
+        final String[] appInstanceId = {""};
+
+        fireBase = new FireBase("", "");
+
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                token[0] = task.getResult();
+                fireBase.setToken(token[0]);
+                Log.e("xujm", "fireBase token---->" + token[0]);
+            } else {
+                fireBase.setToken("-1");
+                Log.e("xujm", "token---->" + task.getException());
+            }
+            sendFireBase();
+        });
+
+        FirebaseAnalytics.getInstance(application).getAppInstanceId().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                appInstanceId[0] = task.getResult();
+                fireBase.setAppInstanceId(appInstanceId[0]);
+                Log.e("xujm", "fireBase appInstanceId---->" + appInstanceId[0]);
+            } else {
+                fireBase.setAppInstanceId("-1");
+                Log.e("xujm", "appInstanceId---->" + task.getException());
+            }
+            sendFireBase();
+        });
+    }
+
+    /**
+     * 发送FireBase
+     */
+    private void sendFireBase() {
+        Log.e("xujm", "sendFireBase---->" + fireBase.toString());
+        if (TextUtils.isEmpty(fireBase.getAppInstanceId()) || TextUtils.isEmpty(fireBase.getToken())) {
+            return;
+        }
+        Intent customIntent = new Intent();
+        customIntent.setAction("com.rupee.park.action.AFRECEIVER");
+        customIntent.putExtra(BR_ACTION, ACTION_FB);
+        customIntent.putExtra(KEY_TOKEN, fireBase.getToken());
+        customIntent.putExtra(KEY_APPINSTANCEID, fireBase.getAppInstanceId());
+        sendBroadcast(customIntent);
+    }
 
     /**
      * 获取进程号对应的进程名
      *
      * @return 进程名
      */
-    public static String getCurrentProcessName() {
-        BufferedReader reader = null;
-        try {
-            int pid = android.os.Process.myPid();
-            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
-            String processName = reader.readLine();
-            if (!TextUtils.isEmpty(processName)) {
-                processName = processName.trim();
-            }
-            return processName;
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException exception) {
-                exception.printStackTrace();
+    private boolean isMainProcess() {
+        int pid = android.os.Process.myPid();
+        String processName = "";
+        ActivityManager mActivityManager = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningAppProcessInfo appProcess : mActivityManager.getRunningAppProcesses()) {
+            if (appProcess.pid == pid) {
+                processName = appProcess.processName;
+                break;
             }
         }
-        return null;
+        String packageName = this.getPackageName();
+        if (processName.equals(packageName)) {
+            return true;
+        }
+        return false;
     }
+
 }
--- app/src/main/java/com/alan/pluginhost/google/MyFirebaseMessagingService.java
+++ app/src/main/java/com/alan/pluginhost/google/MyFirebaseMessagingService.java
@@ -1,19 +1,19 @@
-//package com.alan.pluginhost.google;
-//
-//import androidx.annotation.NonNull;
-//
-//import com.google.firebase.messaging.FirebaseMessagingService;
-//import com.google.firebase.messaging.RemoteMessage;
-//
-//import java.util.Map;
-//
-//public class MyFirebaseMessagingService extends FirebaseMessagingService {
-//    @Override
-//    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-//        Map<String, String> data = remoteMessage.getData();
-//        if (data.size() > 0) {
-//        }
-//        if (remoteMessage.getNotification() != null) {
-//        }
-//    }
-//}
+package com.alan.pluginhost.google;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        if (data.size() > 0) {
+        }
+        if (remoteMessage.getNotification() != null) {
+        }
+    }
+}
